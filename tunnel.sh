7817fd55b9d23938319bd03a1545a7626cda8698
Niccolò Maltoni <nicco.mlt@gmail.com>
2019-06-30 18:57:59 +0200

    feat: add main tunnel script

---
+++ tunnel.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $1 = '-h' ]; then
+  echo Usage: ./tunnel.sh localPort remotePort privateServer userOrEmail [sshTarget]
+else
+  if [ $5 ]; then
+    SSHTARGET=$5
+  else
+    SSHTARGET="alfa.csr.unibo.it"
+  fi
+  ssh -L $1:$3:$2 -N -l $4 $SSHTARGET
+fi
