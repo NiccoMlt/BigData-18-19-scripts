bfed9a16c6fe092c9deec0c6fe40aea022ed4f2a
Niccolò Maltoni <nicco.mlt@gmail.com>
2019-07-06 12:45:10 +0200

    feat: add WebHDFS

---
+++ webhdfs.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if [[ $1 = '-h' ]]; then
+  echo "Usage: ./webhdfs.sh uniboEmail [localPort1] [localPort2]"
+else
+  PORT1=50070
+  if [[ $2 ]]; then
+    LOCALPORT1=$2
+  else
+    LOCALPORT1=${PORT1}
+  fi
+  
+  PORT2=50075
+  if [[ $3 ]]; then
+    LOCALPORT2=$3
+  else
+    LOCALPORT2=${PORT2}
+  fi
+  echo "Opening tunnel for WebHDFS at http://isi-vclust0.csr.unibo.it:$PORT1 opened on port $LOCALPORT1"
+  echo "Also, port $PORT2 tunneled to port $LOCALPORT2"
+  echo "Access web GUI at http://localhost:$LOCALPORT1/cmf/home"
+  
+  ssh -L ${LOCALPORT1}:isi-vclust0.csr.unibo.it:${PORT1} -L ${LOCALPORT2}:isi-vclust0.csr.unibo.it:${PORT2} -N -l $1 alfa.csr.unibo.it
+fi
