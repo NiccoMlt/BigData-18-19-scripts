a124d1c295afef4e3f5d5498edf9b97129e8c314
Niccolò Maltoni <nicco.mlt@gmail.com>
2019-07-26 12:20:38 +0200

    feat: improve YARN script

--- yarnClient.sh
+++ yarnClient.sh
@@ -2,7 +2,7 @@
 
 if [[ $1 = '-h' ]]; then
   echo "Tunnel Yarn remote ports 8088 and 19888 to local host"
-  echo "Usage: ./webhdfs.sh uniboEmail [localPort1] [localPort2]"
+  echo "Usage: ./yarnClient.sh uniboEmail [jobPort (8088)] [detailPort (19888)] [logPort (8042)]"
 else
   PORT1=8088
   if [[ $2 ]]; then
@@ -17,9 +17,17 @@ else
   else
     LOCALPORT2=${PORT2}
   fi
+  
+  PORT3=8042
+  if [[ $4 ]]; then
+    LOCALPORT3=$4
+  else
+    LOCALPORT3=${PORT3}
+  fi
+  
   echo "Opening tunnel for Yarn at http://isi-vclust0.csr.unibo.it:$PORT1 opened on port $LOCALPORT1"
   echo "Also, port $PORT2 tunneled to port $LOCALPORT2"
   echo "Access web GUI at http://localhost:$LOCALPORT1"
   
-  ssh -L ${LOCALPORT1}:isi-vclust0.csr.unibo.it:${PORT1} -L ${LOCALPORT2}:isi-vclust0.csr.unibo.it:${PORT2} -N -l $1 alfa.csr.unibo.it  
+  ssh -L ${LOCALPORT1}:isi-vclust0.csr.unibo.it:${PORT1} -L ${LOCALPORT2}:isi-vclust0.csr.unibo.it:${PORT2} -L ${LOCALPORT3}:isi-vclust0.csr.unibo.it:${PORT3} -N -l "$1" alfa.csr.unibo.it  
 fi
